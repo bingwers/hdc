234cca2f1aec2a1ef42461710a7ac23f4dfda067
LLiu6 <61231770+LLiu6@users.noreply.github.com>
2022-05-03 11:46:40 -0400

    Update example.py

--- example.py
+++ example.py
@@ -13,7 +13,7 @@ model = MNIST_Model(
     imageSize=14 # 28x28 is full size image, works with anything down to 9x9
 )
 
-print("Training... (this might take a few minutes)")
+print("MNIST Training... (this might take a few minutes)")
 model.train(
     trainSamples=60000, # number of train images; 60000 is max # in mnist dataset
     retrainIterations=4, # number of retrain iterations; these are passes through
@@ -21,19 +21,19 @@ model.train(
                          # errors to try to boost accuracy
 )
 
-print("Testing...")
+print("MNIST Testing...")
 nCorrect = model.test(
     testSamples=10000, # number of test images; 10000 is max # in the MNIST dataset
 )
 
-print(f"Test Accuracy: {100 * nCorrect / 10000:.2f}%")
+print(f"MNIST Test Accuracy: {100 * nCorrect / 10000:.2f}%")
 
 # Example Saving and then loading the same model
 
 model.save("models/example1.hvmodel")
 loadedModel = MNIST_Model.load("models/example1.hvmodel")
 nCorrect = loadedModel.test(testSamples=10000)
-print(f"Reloaded Model Test Accuracy: {100 * nCorrect / 10000:.2f}%") # should be the same as before
+print(f"MNIST Reloaded Model Test Accuracy: {100 * nCorrect / 10000:.2f}%") # should be the same as before
 
 # Iterative training: do one training iteration at a time and measure accuracy
 # on the test set after each one; note this model will have different randomly
@@ -41,15 +41,15 @@ print(f"Reloaded Model Test Accuracy: {100 * nCorrect / 10000:.2f}%") # should b
 
 print("\nTraining model with accuracy after each iteration:")
 model2 = MNIST_Model(10000, 2, 2, 28)
-for i in range(5):
+for i in range(10):
     model2.trainOneIteration(60000)
 
     nCorrect = model2.test(10000)
-    print(f"Accuracy after {i+1} iterations: {100 * nCorrect / 10000:.2f}%")
+    print(f"MNIST: Accuracy after {i+1} iterations: {100 * nCorrect / 10000:.2f}%")
 
 # Iterative Training on the ISOLET dataset
 model = ISOLET_Model(10000, 64, 2)
 for i in range(10):
     model.trainOneIteration()
     nCorrect = model.test()
-    print(f"Accuracy after {i}: {nCorrect/1559*100:.2f}%")
+    print(f"ISOLET: Accuracy after {i}: {nCorrect/1559*100:.2f}%")
